f9386e92fb31fd986e08f7cdca3dfc857c78bcbd
Samuel-sys <alencar5647@gmail.com>
2023-08-27 17:36:35 -0300

    Script JS para consulta os pokemons E criar o card com os dados do pokemon

---
+++ assets/js/fethPokemon.js
@@ -0,0 +1,30 @@
+export async function searchPokemon(pokemon) {
+  try {
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+    const response = await fetch(url); //faz o get dos dados do pokemon
+    return await response.json(); //retorna um Json
+  } catch (error) {
+  }
+}
+
+export async function getPokemons(limit = 50, offset = 0) {
+  try {
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+    const response = await fetch(url); //faz o get dos dados do pokemon
+    const data = await response.json(); //retorna um Json
+
+    return {
+      pokemons: await Promise.all(
+        data.results.map(async (pokemon) => {
+          return await searchPokemon(pokemon.name);
+        })
+      ),
+      count: data.count,
+    };
+  } catch (error) {
+    //Caso o pokemon não exista retornamos um objeto informando que o pokemon não existe ou não foi encontrado
+    return { status: `Pokemon não encontrado` };
+  }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,95 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+import { getPokemons } from "./fethPokemon.js";
 
-const maxRecords = 151
-const limit = 10
-let offset = 0;
+const $pokedex = document.querySelector(".pokemons");
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+window.onload = function () {
+  adicionaPokemonsAPokedex();
+};
+
+// Quando o usuario chegar no final da paginan entra no processo para carregar novos pokemons
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const bodyHeight = document.body.scrollHeight;
+  if (scrollPosition > bodyHeight - windowHeight - 100) {
+    adicionaPokemonsAPokedex();
+  }
+});
+
+function cardPokemon(pkm) {
+  //Pegamos os dados do pokemon e imprimimos no corpo do site
+  return `<li class="pokemon ${pkm.type[0].type.name}">
+            <div class="top-pokemon"> <span class="name">${pkm.name}</span>
+                <span class="number">#${pkm.id}</span>
+            </div>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pkm.type
+                      .map(
+                        (x) =>
+                          `<img class="type" src="./assets/img/type/${x.type.name}.png" alt="${x.type.name}" />`
+                      )
+                      .join("")}
                 </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+                <img class="pokemon-img" src="${pkm.img}" alt="${pkm.name}" />
+            </div>                                          
+        </li>`;
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+let acumulado = 0; // ele conta quantas vezes tivemos que fazer uma consulta de pokemon
+let telaCarregando = false; //informa se a tela está carregando ou não
+async function adicionaPokemonsAPokedex() {
+  
+  // se a tela estiver no modo de carregando ele não executa a função para evitar bugs
+  if (telaCarregando) {
+    console.log("Carregando informações...");
+    return;
+  } 
+  
+  indicaTelaCaregando(); // Cria uma tela com uma animação de carregando
+  //Faz a busca de varios pokemons e gera uma lista
+  let pokemons = await getPokemons(50, 50 * acumulado);
+
+  //Se tiver acabado a lista de pokemons ele faz um alert informado ao usuario
+  if(!pokemons.pokemons.length){
+    console.log("Acabou os pokemons")
+    return;
+  }
+
+  //Serve como um controle de loop, eu resolvi fazer tudo como onePage 
+  //então não vamos dividir isso em varias paginas, se o usuario quiser 
+  //ver um pokemon especifico vai ter que pesquisar
+  acumulado++;
+  
+  //Criamos um card para cada pokemon que recebemos da API
+  pokemons.pokemons.map((pkm) => {
+    //Realizamos um loop que irá criar um card para cada pokemon
+    $pokedex.innerHTML += cardPokemon({
+      id: pkm.id,
+      name: pkm.name,
+      type: pkm.types,
+      img: pkm["sprites"]["versions"]["generation-v"]["black-white"][
+        "animated"
+      ]["front_default"],
+    });
+  });
+
+  indicaTelaCaregando(); // Remove a tela de carregando depois de executado todo o processo
 }
 
-loadPokemonItens(offset, limit)
-
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
-
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
-
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
-    }
-})
+function indicaTelaCaregando() {
+  telaCarregando = !telaCarregando;
+  //se estiver carregando as informações ainda ele adiciona a tela de load
+  if (telaCarregando) {
+    let load = document.createElement("div");
+    load.classList.add("loading");
+    load.innerHTML = '<img src="./assets/img/loading.gif" >';
+    document.querySelector("body").appendChild(load);
+  } else {
+    document
+      .querySelector("body")
+      .removeChild(document.querySelector(".loading"));
+  }
+}
